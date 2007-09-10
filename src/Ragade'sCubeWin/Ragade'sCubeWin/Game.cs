8e333ac3cb39e4a12b64b50a097dcd411df1050e
cwlock01 <cwlock01@19a20d19-7738-0410-aeff-ddaf599ae63d>
2007-09-10 18:29:49 +0000

    Initial Check-in of Source. It is just the automatically generated windows XNA game.

---
+++ src/Ragade'sCubeWin/Ragade'sCubeWin/Game.cs
@@ -0,0 +1,110 @@
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+#endregion
+
+namespace Ragade_sCubeWin
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class RagadesCube : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        ContentManager content;
+
+
+        public RagadesCube()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            content = new ContentManager(Services);
+        }
+
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+
+        /// <summary>
+        /// Load your graphics content.  If loadAllContent is true, you should
+        /// load content from both ResourceManagementMode pools.  Otherwise, just
+        /// load ResourceManagementMode.Manual content.
+        /// </summary>
+        /// <param name="loadAllContent">Which type of content to load.</param>
+        protected override void LoadGraphicsContent(bool loadAllContent)
+        {
+            if (loadAllContent)
+            {
+                // TODO: Load any ResourceManagementMode.Automatic content
+            }
+
+            // TODO: Load any ResourceManagementMode.Manual content
+        }
+
+
+        /// <summary>
+        /// Unload your graphics content.  If unloadAllContent is true, you should
+        /// unload content from both ResourceManagementMode pools.  Otherwise, just
+        /// unload ResourceManagementMode.Manual content.  Manual content will get
+        /// Disposed by the GraphicsDevice during a Reset.
+        /// </summary>
+        /// <param name="unloadAllContent">Which type of content to unload.</param>
+        protected override void UnloadGraphicsContent(bool unloadAllContent)
+        {
+            if (unloadAllContent)
+            {
+                // TODO: Unload any ResourceManagementMode.Automatic content
+                content.Unload();
+            }
+
+            // TODO: Unload any ResourceManagementMode.Manual content
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
