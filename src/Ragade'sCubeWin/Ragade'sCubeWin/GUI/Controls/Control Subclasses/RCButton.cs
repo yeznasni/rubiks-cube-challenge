f6a221ec6af818aaf8a7f88feb79e9af544a9c56
steven.shofner <steven.shofner@19a20d19-7738-0410-aeff-ddaf599ae63d>
2007-10-26 21:04:34 +0000

    Added XML to RCButton and started work on RCRadioChannel.

--- src/Ragade'sCubeWin/Ragade'sCubeWin/GUI/Controls/Control Subclasses/RCButton.cs
+++ src/Ragade'sCubeWin/Ragade'sCubeWin/GUI/Controls/Control Subclasses/RCButton.cs
@@ -15,8 +15,23 @@ namespace RagadesCubeWin.GUI
 
         #region        ------------------------------Local Constants
 
+        /// <summary>
+        /// The internal Z-order of the base image, used to determine which images
+        /// are drawn over which other images.  The base image should be on the
+        /// bottom, as it should have the highest opacity.
+        /// </summary>
         protected float BASE_IMAGE_Z_ORDER = 0f;
+        /// <summary>
+        /// The internal Z-order of the focus image, used to determine which images
+        /// are drawn over which other images.  The focus image should probably be 
+        /// in the middle, as it should be less visible than the activating image.
+        /// </summary>
         protected float FOCUSED_IMAGE_Z_ORDER = 0.1f;
+        /// <summary>
+        /// The internal Z-order of the 'pushing' image, used to determine which images
+        /// are drawn over which other images.  The 'pushing' image should probably be 
+        /// on the top, as it should be the most visible effect.
+        /// </summary>
         protected float ACTIVATING_IMAGE_Z_ORDER = 0.3f;
 
         #endregion     ------------------------------Local Constants
@@ -31,28 +46,46 @@ namespace RagadesCubeWin.GUI
         [needsXML]
         private bool _thinksItIsBeingPushed = false;
 
-        [needsXML]
+        /// <summary>
+        /// The image overlay that indicates that the RCButton is focused.
+        /// This should be transparent.
+        /// </summary>
         private RCQuad focusedImageObject = null;
 
-        [needsXML]
+        /// <summary>
+        /// The image overlay that indicates that the RCButton is being pushed.
+        /// This should be transparent.
+        /// </summary>
         private RCQuad activatingImageObject = null;
 
         #endregion ------------------------------Private data members
 
         #region    ------------------------------Events and their delegates
-        [needsXML]
+        /// <summary>
+        /// The type of function that an RCButton's events call; void and parameterless.
+        /// </summary>
         internal delegate void RCButtonEvent();
-        [needsXML]
+        /// <summary>
+        /// The event to throw when the RCButton begins to be pressed.
+        /// </summary>
         internal event RCButtonEvent WhilePressing = null;
-        [needsXML]
+        /// <summary>
+        /// The event to throw when the RCButton begins to be released, if the mouse
+        /// is hovering over it.
+        /// </summary>
         internal event RCButtonEvent WhileReleasing = null;
-        [needsXML]
+        /// <summary>
+        /// The event to throw when the RCButton has been pressed and released.
+        /// </summary>
         internal event RCButtonEvent AfterPressedAndReleased = null;
-        [needsXML]
+        /// <summary>
+        /// The event to throw when the RCButton takes focus.
+        /// </summary>
         internal event RCButtonEvent Focus = null;
-        [needsXML]
+        /// <summary>
+        /// The event to throw when the RCButton loses focus.
+        /// </summary>
         internal event RCButtonEvent UnFocus = null;
-        [needsXML]
 
         #endregion ------------------------------Events and their delegates
 
@@ -72,14 +105,20 @@ namespace RagadesCubeWin.GUI
 
 
         #region    ------------------------------Public properties to return information regarding the state of the button
-        [needsXML]
+        
+        [notFullyImplemented("Currently keeps track of focus internally, instead of looking at actual focusing properties.")]
+        /// <summary>
+        /// Returns true if the RCButton is currently being focused, and false if it is not.
+        /// </summary>
         public bool isFocused
         {
             get
             { return _thinksItHasFocus; }
         }
 
-        [needsXML]
+        /// <summary>
+        /// Returns true if the RCButton is currently depressed, and false if it is not.
+        /// </summary>
         public bool isBeingPushed
         {
             get { return _thinksItIsBeingPushed; }
@@ -87,34 +126,20 @@ namespace RagadesCubeWin.GUI
         #endregion    ------------------------------Public properties to return information regarding the state of the button
 
 
-        #region    ------------------------------Enumerations for RCButton
-        [needsXML]
-        [scaffolding]
-        enum RCButtonState
-        {
-            [needsXML]
-            normal = 0,
-            [needsXML]
-            invisible = 1,
-            [needsXML]
-            disabled = 2,
-            [needsXML]
-            focused = 4,
-            [needsXML]
-            pressed = 8,
-            [needsXML]
-            pressing = 16
-        }
-        #endregion ------------------------------Enumerations for RCButton
-
-
         
         
 
 
-
+        /// <summary>
+        /// Creates a new RCButton object.  This needs to be attached to something for it to be visible.
+        /// Note that this is a 'flat 3-d' object; it has a very small 'length' property by definition.
+        /// </summary>
+        /// <param name="width">The width of the object in world-coordinates.</param>
+        /// <param name="height">The height of the object in world-coordinates.</param>
+        /// <param name="screenWidth">The width of the object in screen-coordinates.</param>
+        /// <param name="screenHeight">The height of the object in screen-coordinates.</param>
+        /// <param name="Font">The BitmapFont that the button's text uses.</param>
         [placeHolder]
-        [needsXML]
         internal RCButton(
             float width, 
             float height,
@@ -150,7 +175,7 @@ namespace RagadesCubeWin.GUI
 
         #region    ------------------------------Overridden functions
 
-        [needsXML]
+
         public override void LoadGraphicsContent(GraphicsDevice graphics, Microsoft.Xna.Framework.Content.ContentManager content)
         {
             base.LoadGraphicsContent(graphics, content);
@@ -167,7 +192,11 @@ namespace RagadesCubeWin.GUI
 
         #endregion ------------------------------Overridden functions
 
-        [needsXML]
+
+        /// <summary>
+        /// The internal logic associated with focusing.  Includes animation and setting of local variables.
+        /// You should never have to mess with this, but it's public in case you want to.
+        /// </summary>
         public void FocusInternalLogic()
         {
             _thinksItHasFocus = true;
@@ -177,14 +206,16 @@ namespace RagadesCubeWin.GUI
             }
         }
 
-        [needsXML]
+        /// The internal logic associated with unfocusing.  Includes animation and setting of local variables.
+        /// You should never have to mess with this, but it's public in case you want to.
         public void UnFocusInternalLogic()
         {
             _thinksItHasFocus = false;
             RemoveChild(focusedImageObject);
         }
 
-        [needsXML]
+        /// The internal logic associated with depressing.  Includes animation and setting of local variables.
+        /// You should never have to mess with this, but it's public in case you want to.
         public void PressingInternalLogic()
         {
             _thinksItIsBeingPushed = true;
@@ -194,7 +225,8 @@ namespace RagadesCubeWin.GUI
             }
         }
 
-        [needsXML]
+        /// The internal logic associated with un-depressing.  Includes animation and setting of local variables.
+        /// You should never have to mess with this, but it's public in case you want to.
         public void UnPressingInternalLogic()
         {
             _thinksItIsBeingPushed = false;
@@ -202,11 +234,13 @@ namespace RagadesCubeWin.GUI
         }
 
         [needsXML]
+        [placeHolder]
         protected override void instantiateBaseAndCurrentImageObjects(float width, float height, int screenWidth, int screenHeight)
         {
             baseImageObject = new RCQuad(width, height, screenWidth, screenHeight);
             //currentImageObject = new RCQuad(width, height, screenWidth, screenHeight);
         }
+
 
         public override bool OnEvent(GUIEvent guiEvent)
         {
