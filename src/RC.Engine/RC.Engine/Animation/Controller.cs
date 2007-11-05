165c7bb5b98e2de7f4470e82ff7f2cc043ab309b
ctboyle <ctboyle@19a20d19-7738-0410-aeff-ddaf599ae63d>
2007-11-05 00:12:03 +0000

    Added animation controller files.

---
+++ src/RC.Engine/RC.Engine/Animation/Controller.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using RagadesCubeWin.GraphicsManagement;
+
+
+namespace RagadesCubeWin.Animation
+{
+    public interface IController
+    {
+        bool IsAnimating { get; }
+        void Update(GameTime gameTime);
+
+        RCSpatial Parent { get;}
+
+    }
+
+
+    public abstract class Controller<CntrlType> : IController where CntrlType : RCSpatial
+    {
+        protected CntrlType _controlledItem;
+        protected bool _isAnimating;
+                
+        public RCSpatial Parent
+        {
+            get { return _controlledItem; }
+        }
+
+        public bool IsAnimating 
+        {
+            get { return _isAnimating; }
+        }
+
+
+        public bool AttachToObject(CntrlType parent)
+        {
+            bool fSuccess = false;
+            if (parent != null)
+            {
+                _controlledItem = parent;
+                fSuccess = _controlledItem.AddController(this);
+            }
+
+            return fSuccess;
+        }
+
+
+        public Controller()
+        {
+            _controlledItem = null;
+            _isAnimating = false;
+        }
+
+        
+        public abstract void Update(GameTime gameTime);
+    }
+}
