241d37a763e93950864554554f12723fccde50e4
jesprl@hotmail.com <jesprl@hotmail.com@19a20d19-7738-0410-aeff-ddaf599ae63d>
2007-11-07 16:52:40 +0000

    Changed music beat Added NUnit project for testing.

---
+++ trunk/src/NUnitTest/MainTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest
+{
+    class MainTest
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            InputTest IT = new InputTest();
+
+           
+        }
+    }
+}
