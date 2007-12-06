dfc245896e5c7697f463f62c85c3d308753fbb81
ctboyle <ctboyle@19a20d19-7738-0410-aeff-ddaf599ae63d>
2007-12-06 05:51:17 +0000

    Editing names of build files attributes.

--- trunk/src/RC.Gui/RC.Gui/Properties/AssemblyInfo.cs
+++ trunk/src/RC.Gui/RC.Gui/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RC.Gui")]
-[assembly: AssemblyProduct("RC.Gui")]
+[assembly: AssemblyTitle("Ragade's Cube GUI")]
+[assembly: AssemblyProduct("Ragade's Cube")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LBSS")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- trunk/src/RC.Input/RC.Input/Properties/AssemblyInfo.cs
+++ trunk/src/RC.Input/RC.Input/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RC.Input")]
-[assembly: AssemblyProduct("RC.Input")]
+[assembly: AssemblyTitle("Ragade's Cube Input")]
+[assembly: AssemblyProduct("Ragade's Cube")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LBSS")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
